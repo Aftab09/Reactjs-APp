b843b68077632f8889d061a14719f6bb51c5ea36
Aftab Gori <aftabgori2002@gmail.com>
2022-05-25 21:03:34 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,86 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
-function App() {
+function App() {  
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [address, setAddress] = useState("");
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); 
+    // console.log(name, email, address);
+
+    const DisplayInputData  = {name, email, address}
+
+    if(name && email && address){
+      setList((ls) => [...ls, DisplayInputData]);
+      setName("");
+      setEmail("");
+      setAddress("");
+
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="form">
+        <form onSubmit={handleSubmit}>
+        <h1>Add Users</h1>
+        <label>
+          Name 
+        </label>
+        <input
+          type="text"
+          placeholder='Enter Name'
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          name="name"
+          id="InputName"
+        />
+        <br></br>
+           <label>
+          Email 
+        </label>
+        <input
+          type="text"
+          placeholder='Enter Email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          name="email"
+          id="InputEmail"
+        />
+        <br></br>
+           <label>
+          Address 
+        </label>
+        <textarea
+          type="text"
+          placeholder='Enter Address'
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
+          name="address"
+          id="InputAddress"
+        />
+        <button type='submit' className='btn'>Submit</button>
+        </form>
+      </div>
+
+      <div className='Display-Users'>
+    <h1>Display Users</h1>
+    {
+          list.map((a) => <div>
+            <ul>
+            <li><b><u>Name:-</u></b> {a.name}</li>
+            <li><b><u>Email:-</u></b> {a.email}</li>
+            <li><b><u>Address:-</u></b> {a.address}</li>
+            </ul>
+          </div>)
+        }
     </div>
+
+    </div> 
   );
 }
 
---
+++ src/OutPut.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css'
+import App from "./App";
+
+function OutPut(){
+    return(
+        <div className="output">
+            <section className="output-section"
+                
+            />
+        </div>
+    )
+}
+
+export default OutPut;
